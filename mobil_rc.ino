528941762171572fa502a5e07affcc4c48cd58ef
karuego <66974423+karuego@users.noreply.github.com>
2024-12-18 03:06:18 +0800

    refactor

--- mobil_rc.ino
+++ mobil_rc.ino
@@ -1,23 +1,15 @@
-#include <avr/io.h>
 #include <SoftwareSerial.h>
 
-#define LMN 6
-#define LMP 7
-#define RMN 8
-#define RMP 9
-
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(2, 3);
+const uint8_t pins[] = {6, 7, 8, 9, 13};
 
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
   Serial.println("Mulai");
-
-  DDRB |= (1 << DDB5), PORTB &= ~(1 << PORTB5);
-  DDRD |= 0xC0, DDRB |= 0x03;
-  PORTD &= ~0xC0, PORTB &= ~0x03;
-  pinMode(12, OUTPUT);
-  digitalWrite(12, HIGH);
+  
+  for (const pin : pins)
+    pinMode(pin, OUTPUT);
 }
 
 void loop() {
@@ -27,19 +19,17 @@ void loop() {
   Serial.println(arah);
 
   switch (arah) {
-    case 'F': motor(0b1001); break;
-    case 'B': motor(0b0110); break;
-    case 'L': motor(0b0101); break;
-    case 'R': motor(0b1010); break;
-    case 'W': digitalWrite(13, HIGH); break;
-    case 'w': digitalWrite(13, LOW); break;
-    default: motor(0b0000); break;
+    case 'F': motor(0b0101); break;
+    case 'B': motor(0b1010); break;
+    case 'L': motor(0b1001); break;
+    case 'R': motor(0b0110); break;
+    case 'W': digitalWrite(pins[4], HIGH); break;
+    case 'w': digitalWrite(pins[4], LOW); break;
+    default: motor(0b0000);
   }
 }
 
-void motor(uint8_t states) {
-  digitalWrite(LMP, (states >> 0) & 0x01);
-  digitalWrite(LMN, (states >> 1) & 0x01);
-  digitalWrite(RMP, (states >> 2) & 0x01);
-  digitalWrite(RMN, (states >> 3) & 0x01);
+void motor(uint8_t val) {
+  for (uint8_t i = 0; i < 5; i++)
+    digitalWrite(pins[i], (val >> i) & 1);
 }
